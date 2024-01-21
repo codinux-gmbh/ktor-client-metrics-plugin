74146a10918d1e1f02a4c67fa2a9cf3bedfd516b
dankito <git@dankito.net>
2024-01-21 19:50:37 +0100

    Extracted MetricsPluginConfig to an extra file

---
+++ ktor-client-metrics/src/commonMain/kotlin/net/codinux/web/ktor/client/metrics/MetricsPluginConfig.kt
@@ -0,0 +1,36 @@
+package net.codinux.web.ktor.client.metrics
+
+import io.ktor.http.*
+import io.ktor.util.*
+
+class MetricsPluginConfig {
+    lateinit var meterRegistry: MeterRegistry
+
+    /**
+     * Add additional tags to metrics
+     */
+    var additionalTags: Map<String, String> = emptyMap()
+
+    /**
+     * Adjust the value of the "uri" tag.
+     * Return `null` if the tag value for this Url should be set to the default value [Url.encodedPath].
+     */
+    var getUriTag: ((Url) -> String?)? = null
+
+    var configureTags: ((standardTags: MutableMap<String, String>, HttpMethod, Url, status: Int, Attributes, throwable: Throwable?) -> Unit)? = null
+
+    data class AppliedConfig(
+        val meterRegistry: MeterRegistry,
+        val additionalTags: Map<String, String>,
+        val getUriTag: ((Url) -> String?)?,
+        val configureTags: ((standardTags: MutableMap<String, String>, HttpMethod, Url, status: Int, Attributes, throwable: Throwable?) -> Unit)?
+    )
+
+    internal fun applyConfig(): AppliedConfig {
+        if (this::meterRegistry.isInitialized == false) {
+            throw IllegalArgumentException("meterRegistry must be set")
+        }
+
+        return AppliedConfig(meterRegistry, additionalTags, getUriTag, configureTags)
+    }
+}
